44605661ae4c0d67690a9c139acc7efff7107852
Holozoa <32681354+Holozoa@users.noreply.github.com>
2023-09-25 13:28:51 -0500

    Update disk_vertification_alert.sh

--- disk_vertification_alert.sh
+++ disk_vertification_alert.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
 
-   ```bash
-   #!/bin/bash
-
-   # Check if your devices are present (modify this part according to your needs)
-   if [ -e /dev/sdX ] && [ -e /dev/sdY ]; then
-       echo "Devices are present."
-   else
-       echo "Devices are not found."
-       # Trigger a notification (you can use notify-send or any other preferred method)
-       notify-send "Device Alert" "Standard hard drives not found during boot."
+# Check if your devices are present (modify this part according to your needs)
+if [ -e /dev/sdX ] && [ -e /dev/sdY ]; then
+    echo "Devices are present."
+else
+    echo "Devices are not found."
+    # Trigger a notification (you can use notify-send or any other preferred method)
+    notify-send "Device Alert" "Standard hard drives not found during boot."
+fi
